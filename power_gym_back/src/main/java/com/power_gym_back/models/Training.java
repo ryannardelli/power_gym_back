cff8ae09f3ea0fa47671490bc1d21b93743bfe51
Ryan Nardelli <ryannardelli@gmail.com>
2025-11-25 16:00:34 -0300

    chore: New Entities added to project. Was create twice entity traning and user

---
+++ power_gym_back/src/main/java/com/power_gym_back/models/Training.java
@@ -0,0 +1,33 @@
+package com.power_gym_back.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table(name = "training")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Training {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotBlank
+    @Size(min = 4, max = 120)
+    private String name;
+
+    @Size(min = 12, max = 200 )
+    private String howMake;
+
+    @Column(nullable = false, unique = true, length = 500)
+    private String urlImage;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
