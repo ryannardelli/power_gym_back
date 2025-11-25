133123a8c4010a991c30a33b8106a5d711c7f3b5
Ryan Nardelli <ryannardelli@gmail.com>
2025-11-25 20:07:05 -0300

    New configuration added to configuration cors to apply use of endpoints

---
+++ power_gym_back/src/main/java/com/power_gym_back/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.power_gym_back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // todas as rotas
+                        .allowedOrigins("http://10.0.2.2:5173", "http://10.0.2.2:8080" )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
