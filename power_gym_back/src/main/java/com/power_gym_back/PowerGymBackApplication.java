c32b350ad01726b0f3cbc589783c4de3175e436a
Ryan Nardelli <ryannardelli@gmail.com>
2025-11-25 13:48:48 -0300

    chore: Initilizer Project with archives on spring

---
+++ power_gym_back/src/main/java/com/power_gym_back/PowerGymBackApplication.java
@@ -0,0 +1,13 @@
+package com.power_gym_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerGymBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerGymBackApplication.class, args);
+	}
+
+}
